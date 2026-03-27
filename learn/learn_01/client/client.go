cca2063b34c46f80b01f9a5148f2ce101dc4c898
Fogzan <fogzan@yandex.ru>
2026-03-27 18:51:12 +0300

    upload server&client

--- learn/learn_01/client/client.go
+++ learn/learn_01/client/client.go
@@ -15,7 +15,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	defer conn.Close()
+	// defer conn.Close() перенес в горутину
 
 	var wg sync.WaitGroup
 	wg.Add(1)
@@ -25,34 +25,63 @@ func main() {
 			buff := make([]byte, 1024)
 			n, err := conn.Read(buff)
 			if err != nil {
+				wg.Done()
 				break
 			}
 			if string(buff[0:n]) != "true" {
 				fmt.Print(string(buff[0:n]))
 			} else {
-				wg.Done()
 				conn.Close()
+				wg.Done()
 			}
 		}
 	}()
 
-	fmt.Print("Общий чат: \n")
+	reader := bufio.NewReader(os.Stdin)
+InputUsernameFor:
+	for {
+		fmt.Printf("Введите никнейм: \n> ")
+		myUserName, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Некорректный ввод", err)
+		}
+		if strings.TrimSpace(myUserName) == "" {
+			fmt.Printf("Некорректный ввод\n")
+		} else {
+			n, err := conn.Write([]byte(myUserName))
+			if n == 0 || err != nil {
+				fmt.Println(err)
+				return
+			}
+			break InputUsernameFor
+		}
+	}
+
+	fmt.Printf("\nОбщий чат: \n")
 
 	for {
-		reader := bufio.NewReader(os.Stdin)
 		source, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Некорректный ввод", err)
 			continue
 		}
 		// отправляем сообщение серверу
-
 		if n, err := conn.Write([]byte(source)); n == 0 || err != nil {
 			fmt.Println(err)
-			return
+			continue
 		}
 
 		if strings.TrimSpace(source) == "/exit" {
+			// buff := make([]byte, 1024)
+			// n, err := conn.Read(buff)
+			// if err != nil {
+			// 	break
+			// }
+			// if string(buff[0:n]) == "true" {
+			// 	break
+			// } else {
+			// 	continue
+			// }
 			break
 		}
 
--- learn/learn_01/server/server.go
+++ learn/learn_01/server/server.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"sync"
+	"time"
 )
 
 type connectedUser struct {
-	socket  net.Conn
-	address net.Addr
-	id      int
+	socket   net.Conn
+	address  net.Addr
+	id       int
+	userName string
 }
 
 func main() {
@@ -22,6 +25,7 @@ func main() {
 	defer listener.Close()
 	fmt.Println("Server is listening...")
 
+	var mute sync.Mutex
 	connectedClient := []connectedUser{}
 	idClient := 0
 
@@ -32,19 +36,41 @@ func main() {
 			conn.Close()
 			return
 		}
-		go processingConnection(conn, &connectedClient, idClient)
+		go processingConnection(conn, &connectedClient, idClient, &mute)
 		idClient++
 	}
 }
 
-func processingConnection(conn net.Conn, connectedClient *[]connectedUser, idClient int) {
+func processingConnection(conn net.Conn, connectedClient *[]connectedUser, idClient int, mute *sync.Mutex) {
+	// Доп часть
 	defer conn.Close()
-	defer fmt.Printf("Количество клиентов: %v\n\n", len(*connectedClient))
-	defer fmt.Printf("\n[Пользователь %v] Отключился\n", conn.LocalAddr())
-	fmt.Printf("\n[Пользователь %v] Подключился\n", conn.LocalAddr())
-
-	*connectedClient = append(*connectedClient, connectedUser{socket: conn, address: conn.LocalAddr(), id: idClient})
 	defer func() {
+		mute.Lock()
+		fmt.Printf("Количество клиентов: %v\n\n", len(*connectedClient))
+		mute.Unlock()
+	}()
+	defer fmt.Printf("\n[Пользователь %v] Отключился\n", conn.RemoteAddr())
+	fmt.Printf("\n[Пользователь %v] Подключился\n", conn.RemoteAddr())
+
+	// Получение username
+	input := make([]byte, (1024 * 4))
+	n, err := conn.Read(input)
+	if n == 0 || err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	getUserName := strings.TrimSpace(string(input[:n]))
+	fmt.Printf("Пользователь: %v - назвался: %v", conn.RemoteAddr(), getUserName)
+
+	// Добавление клиента в массив
+	mute.Lock()
+	*connectedClient = append(*connectedClient, connectedUser{socket: conn, address: conn.RemoteAddr(), id: idClient, userName: getUserName})
+	mute.Unlock()
+
+	// функция которая выполняется после разрыва соеденения с клиентом
+	defer func() {
+		mute.Lock()
+		defer mute.Unlock()
 		res := -1
 		for n, item := range *connectedClient {
 			if item.id == idClient {
@@ -58,9 +84,14 @@ func processingConnection(conn net.Conn, connectedClient *[]connectedUser, idCli
 		}
 	}()
 
+	// Вывод количества клиентов
+	mute.Lock()
 	fmt.Printf("Количество клиентов: %v\n\n", len(*connectedClient))
+	mute.Unlock()
 
+	// Основной цикл
 	for {
+		// Получение сообщения
 		input := make([]byte, (1024 * 4))
 		n, err := conn.Read(input)
 		if n == 0 || err != nil {
@@ -68,25 +99,58 @@ func processingConnection(conn net.Conn, connectedClient *[]connectedUser, idCli
 			return
 		}
 		sms := string(input[:n])
-		fmt.Printf("Пользователь: %v - оставил сообщение: %v", conn.LocalAddr(), sms)
+		if strings.TrimSpace(sms) != "" {
+			fmt.Printf("Пользователь: %v - оставил сообщение: %v", conn.RemoteAddr(), sms)
+		}
 
-		if strings.TrimSpace(sms) == "/exit" {
+		// Обработка полученного сообщения
+		switch strings.TrimSpace(sms) {
+		case "/exit":
 			_, err := conn.Write([]byte("true"))
 			if err != nil {
 				fmt.Println(err)
 			}
 			return
-		}
-
-		for _, item := range *connectedClient {
-			if item.id != idClient {
-				_, err := item.socket.Write([]byte(sms))
-				if err != nil {
-					fmt.Println(err)
+		case "/list":
+			clientList := "Пользователи в чате: "
+			if len(*connectedClient) < 2 {
+				clientList = "Ты в чате один!\n"
+			} else {
+				mute.Lock()
+				for _, item := range *connectedClient {
+					if item.id != idClient {
+						clientList += item.userName + " "
+					}
 				}
+				mute.Unlock()
+				clientList += "\n"
 			}
 
+			_, err := conn.Write([]byte(clientList))
+			if err != nil {
+				fmt.Println(err)
+			}
+		case "":
+			continue
+		default:
+			mute.Lock()
+			if len(*connectedClient) < 2 {
+				mute.Unlock()
+				continue
+			}
+			for _, item := range *connectedClient {
+				if item.id != idClient {
+					item.socket.SetWriteDeadline(time.Now().Add(30 * time.Second))
+					_, err := item.socket.Write([]byte("\t\t" + getUserName + ": " + sms))
+					if err != nil {
+						fmt.Println(err)
+					}
+					item.socket.SetWriteDeadline(time.Time{})
+				}
+			}
+			mute.Unlock()
 		}
+
 	}
 
 }
